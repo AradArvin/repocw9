fa65804b2e59d86b9e236e4af6a1b09997683f2f
Arad <alirezaarvin95@gmail.com>
2023-06-02 15:09:30 +0330

    added while loop

--- regex_pass.py
+++ regex_pass.py
@@ -1,10 +1,12 @@
 import re
 
-password = str(input("Enter your password(one upper,one lower,and one number at least): "))
-
-check_pass = re.fullmatch(r"[A-Za-z0-9]{8,}", password)
-
-if check_pass:
-    print("Password is valid.")
-else:
-    print("Password is invalid!")
+while True:
+    password = str(input("Enter your password(one upper,one lower,and one number at least),q to exit: "))
+    check_pass = re.fullmatch(r"[A-Za-z0-9]{8,}", password)
+    if password:
+        if check_pass:
+            print("Password is valid.")
+        else:
+            print("Password is invalid!")
+    elif "q":
+        break
