58f48d287c5c43bf596dfd709def3e411a098098
Arad <alirezaarvin95@gmail.com>
2023-06-02 15:13:28 +0330

    program fixed

--- regex_pass.py
+++ regex_pass.py
@@ -2,11 +2,15 @@ import re
 
 while True:
     password = str(input("Enter your password(one upper,one lower,and one number at least),q to exit: "))
-    check_pass = re.fullmatch(r"[A-Za-z0-9]{8,}", password)
-    if password:
-        if check_pass:
-            print("Password is valid.")
-        else:
-            print("Password is invalid!")
-    elif "q":
+    check_pass = re.findall(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$", password)
+    
+    if password == "q":
         break
+
+    if check_pass:
+        print("Password is valid.")
+    else:
+        print("Password is invalid!")
+
+    
+
