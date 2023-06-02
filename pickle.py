bf110f41b270e33bfe8f8f8d57fa4465456a6869
Arad <alirezaarvin95@gmail.com>
2023-06-02 16:17:08 +0330

    q2 end

--- pickle.py
+++ pickle.py
@@ -6,19 +6,21 @@ class Person:
         self.age = age
         self.adress = adress
 
-
-
-
 p1 = Person("arad",20,"tehran")
 p2 = Person("foo",45,"avangard")
 p3 = Person("joojoo",18,"shiraz")
 person_list = [p1,p2,p3] 
 
-def pickleing():
-    with open("pickeled.pkl", "wb") as f:
-        pickle.dump(person_list, f)
+def pickleing(list_of_ppl):
+    with open("pickeled.pickle", "wb") as f:
+        pickle.dump(list_of_ppl, f)
 
 def unpickleing():
-    with open("pickeled.pkl", "rb") as f:
+    with open("pickeled.pickle", "rb") as f:
         loaded_list = pickle.load(f)
         print(loaded_list)
+        for i in range(len(loaded_list)):
+            print(loaded_list[i].name, loaded_list[i].age, loaded_list[i].adress)
+
+pickleing(person_list)
+unpickleing()
